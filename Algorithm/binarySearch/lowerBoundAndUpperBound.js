7795b4df58d9ba789c759f76b7af68368bda1779
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-06-15 14:55:25 +0900

    Create lowerBoundAndUpperBound.js

---
+++ Algorithm/binarySearch/lowerBoundAndUpperBound.js
@@ -0,0 +1,21 @@
+// 정렬된 순서를 유지하면서 배열에 삽입할 가장 왼쪽 인덱스 반환
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
+    // 최대한 왼쪽으로 이동
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1;
+  }
+  return end;
+}
+
+// 정렬된 순서를 유지하면서 배열에 삽입할 가장 오른쪽 인덱스 반환
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    // 최대한 오른쪽으로 이동
+    else start = mid + 1;
+  }
+  return end;
+}
